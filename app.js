814f1dd943e0a3e582c993f4d6772dda66c2b5f0
ricardoyp <ricardoyp07@gmail.com>
2023-12-19 13:13:21 +0100

    Autenticacion

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const { create } = require('express-handlebars');
+const hbs = create({
+  extname: 'hbs', 
+  defaultLayout: 'main', 
+  partialsDir: 'views/partials',
+  // helpers: require('./utils/helpers')
+});
+const session = require('express-session'); 
+const passport = require('passport'); 
+const morgan = require('morgan'); 
+const flash = require('connect-flash');
+const methodOverride = require('method-override')
 
-app.use(express.json()); // Para parsear datos del body en formato.json
+app.use(morgan('dev')); 
+app.use(session({
+    secret: 'tu_clave_secreta',
+    resave: false,
+    saveUninitialized: false
+}));
 
-// Import routes from routes file and mount them
-const router = require('./routes');
-app.use('/', router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(express.static('public')); 
+app.use(flash()); 
+app.use(methodOverride('_method'))
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+require('./config/passport'); 
+
+app.use("/", require("./routes"));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
